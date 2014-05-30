c6b90f3601cef43d51f73cccbb5a0fdd6b9a3324
Anton Aderum <antonaderum@gmail.com>
2014-05-30 13:06:12 +0100

    Copy addon_english and addoninfo

--- ModMaker/modbuilder.cpp
+++ ModMaker/modbuilder.cpp
@@ -1,5 +1,7 @@
 #include "modbuilder.h"
 #include <direct.h>
+#include <iostream>
+#include <fstream>
 #define MakeDir _mkdir
 
 modbuilder::modbuilder(char* modPath, char* modName) {
@@ -28,6 +30,51 @@ void modbuilder::makeDirSecond(char* inDir, char* inDirSec)
 	strcat(result, inDirSec);
 
 	MakeDir(result);
+}
+
+void modbuilder::getPathFirst(char* inBuff, char* indDir)
+{
+	memset(inBuff, 0, FILENAME_MAX);
+	strcpy(inBuff, path);
+	strcat(inBuff, name);
+	strcat(inBuff, indDir);
+}
+
+void modbuilder::getBasePath(char* inBuff)
+{
+	memset(inBuff, 0, FILENAME_MAX);
+	strcpy(inBuff, path);
+	strcat(inBuff, name);
+}
+
+int modbuilder::copyFiles()
+{
+	try
+	{
+		char file_path[FILENAME_MAX];
+
+		getBasePath(file_path);
+		strcat(file_path, "\\addoninfo.txt");
+		ifstream  src("data\\addoninfo.txt", std::ios::binary);
+		ofstream  dst(file_path, std::ios::binary);
+		dst << src.rdbuf();
+		src.close();
+		dst.close();
+
+		getPathFirst(file_path, resource);
+		strcat(file_path, "\\addon_english.txt");
+		src = ifstream("data\\addon_english.txt", std::ios::binary);
+		dst = ofstream(file_path, std::ios::binary);
+		dst << src.rdbuf();
+		src.close();
+		dst.close();
+
+		return 0;
+	}
+	catch (int e)
+	{
+		return -1;
+	}
 }
 
 
@@ -79,6 +126,10 @@ int modbuilder::buildMod()
 		return -1;
 	}
 	if (makeSecondLevelFolders() != 0)
+	{
+		return -1;
+	}
+	if (copyFiles() != 0)
 	{
 		return -1;
 	}
